7aa5727ac5e44a2740bc5e731eb2fb16fed4dd0c
jytaloramon <ramongoncalves76@gmail.com>
2020-01-01 02:13:49 -0300

    Refatoração e add. comentário da estrutura.

--- Queue/main.c
+++ Queue/main.c
@@ -1,13 +1,12 @@
 /**
- * Uma Fila é uma estrutura linear que segue uma ordem específica na qual as operações são executadas. 
- * A ordem é First In First Out (FIFO): o primeiro elemento a ser colocado na estrutura é o primeiro a ser retirado.
- */
+ * Uma Fila é uma estrutura linear que segue uma ordem específica na qual as operações são executadas em ambas extremidades. 
+ * Uma extremidade é sempre usada para inserir dados (enfileirar) e a outra é usada para remover dados (enfileirar). 
+ *      A fila segue a metodologia First-In-First-Out, ou seja, o item de dados armazenado primeiro será acessado primeiro.
+ * Complexidade de alteração: O(1)
+ * */
 
 #include "stdio.h"
 #include "stdlib.h"
-
-#define TRUE 1
-#define FALSE 0
 
 typedef struct _node {
     int value;
@@ -15,7 +14,7 @@ typedef struct _node {
 } Node;
 
 typedef struct _queue {
-    struct _node *root;
+    struct _node *root, **last;
 } Queue;
 
 Node *createNode(int value);
@@ -24,53 +23,57 @@ int empty(Queue *queue);
 
 void enqueue(Queue *queue, Node *node);
 
-Node* desqueue(Queue *queue);
+Node *desqueue(Queue *queue);
 
-Node* getFront(Queue *queue);
+Node *getFront(Queue *queue);
+
+void printArr(Queue *queue);
 
 int main(int argc, char const *argv[]){
     
-    Queue queue = { NULL };
-    Node *nodeAux;
     int opt, value;
-
+    Queue queue = { NULL, &queue.root };
+    Node *nodeAux;
+    
     do {
-        printf("[1] Enqueue\n[2] Dequeue\n[3] fistElQueue\n[Q] Exit\n");
+        printf("[1] Enqueue\n[2] Dequeue\n[3] fistElQueue\n[Q] Exit\nDigite: ");
         scanf("%d", &opt);
 
         switch (opt){
-            case 1:
-                printf("Enqueue value: ");
-                scanf("%d", &value);
+        case 1:
+            printf("Enqueue value: ");
+            scanf("%d", &value);
 
-                nodeAux = createNode(value);
-                enqueue(&queue, nodeAux);
-                break;
+            nodeAux = createNode(value);
+            enqueue(&queue, nodeAux);
+            printArr(&queue);
+            break;
         
-            case 2:
-                if (empty(&queue) == TRUE)
-                    printf("    Pilha Vazia\n");
-                else {
-                    nodeAux = desqueue(&queue);
-                    printf("    Elemento removido: %d\n", nodeAux->value);
-                }
-                break;
+        case 2:
+            if (empty(&queue))
+                printf("    Pilha Vazia\n");
+            else {
+                nodeAux = desqueue(&queue);
+                printf("    Elemento removido: %d\n", nodeAux->value);
+                printArr(&queue);
+            }
+            break;
+        
+        case 3:
+            if (empty(&queue))
+                printf("    Pilha Vazia\n");
+            else {
+                nodeAux = getFront(&queue);
+                printf("    Primeiro elemento: %d\n", nodeAux->value);
+            }
+            break;
             
-            case 3:
-                if (empty(&queue) == TRUE)
-                    printf("    Pilha Vazia\n");
-                else {
-                    nodeAux = getFront(&queue);
-                    printf("    Primeiro elemento: %d\n", nodeAux->value);
-                }
-                break;
-                
-            default:
-                exit(0);
+        default: exit(0);
+
         }
 
         printf("\n"); 
-    } while (TRUE);
+    } while (1);
 
     return 0;
 }
@@ -88,26 +91,33 @@ int empty(Queue *queue){
 }
 
 void enqueue(Queue *queue, Node *node){
-    Node *ptr = queue->root;
-
-    if (ptr == NULL){
-        queue->root = node;
-        return;
-    }
-
-    while (ptr->next != NULL)
-        ptr = ptr->next;
-    
-    ptr->next = node;
+    *(queue->last) = node;
+    queue->last = &node->next;
 }
 
 Node* desqueue(Queue *queue){
     Node *ptr = queue->root;
     queue->root = ptr->next;
     
+    if (queue->root == NULL)
+        queue->last = &queue->root;
+
     return ptr;
 }
 
 Node* getFront(Queue *queue){
     return queue->root;
+}
+
+void printArr(Queue *queue){
+    Node *el = queue->root;
+
+    printf("--------------------\n");
+    
+    while (el != NULL){
+        printf("%d ", el->value);
+        el = el->next;
+    }
+
+    printf("\n--------------------\n\n");
 }
