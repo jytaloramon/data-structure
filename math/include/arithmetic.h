5f4e99269318c08397d122e981bcce594b9beb05
Ramon Almeida <ramongoncalves76@gmail.com>
2021-06-15 21:54:09 -0300

    Added math Libs

---
+++ math/include/arithmetic.h
@@ -0,0 +1,12 @@
+/**
+ * @author Ytalo Ramon
+ * @date   12/06/2021
+*/
+
+/**
+ * Calculate modular arithmetic.
+ * @param a value
+ * @param b div_value
+ * @return remainder
+*/
+int divmodular(int a, int b);
