6c5153467ff89a51a679ed268f8682c7cec808ae
Ramon Almeida <ramogoncalves76@gmail.com>
2021-11-09 12:33:34 -0300

    Added bitwise operators: arithmetic substitution.

--- includes/itree_array.h
+++ includes/itree_array.h
@@ -3,6 +3,6 @@
  * @date   27/06/2021
 */
 
-#define FATHER(v)((((v) - 1) / 2))
-#define CHILDLEFT(v)(((v) * 2 + 1))
-#define CHILDRIGHT(v)(((v) * 2 + 2))
+#define FATHER(v)(((v) - 1) >> 1)
+#define CHILDLEFT(v)(((v) << 1) + 1)
+#define CHILDRIGHT(v)(((v) << 1) + 2)
