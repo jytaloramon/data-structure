b7dd18178ad4f4327d4e5e1b07fe30379285ab53
Ramon Almeida <ramongoncalves76@gmail.com>
2021-11-11 00:19:36 -0300

    Quick classification implementation completed.

--- sort/src/main_sort.c
+++ sort/src/main_sort.c
@@ -8,19 +8,42 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define SIZE 9
+
+int comp(void *const a, void *const b);
+
+void arr_int_show(int *arr, size_t lenght);
+
+int main(int argc, char const *argv[]) {
+
+    int arr[SIZE] = {9, -3, 5, 2, 6, 8, -6, 1, 3};
+
+    printf("+++++ SORT +++++\n\n");
+
+    printf("Source: ");
+    arr_int_show(arr, SIZE);
+
+    // printf("@@ BUBBLE SORT\n");
+    // bubble_sort(arr, 9, sizeof(int), comp);
+
+    printf("@@ QUICK SORT\n");
+    quick_sort(arr, SIZE, sizeof(int), comp);
+
+    printf("Final: ");
+    arr_int_show(arr, SIZE);
+
+    return 0;
+}
+
 int comp(void *const a, void *const b) {
     int v = *((int *)a), u = *((int *)b);
     return v - u;
 }
 
-int main(int argc, char const *argv[]) {
+void arr_int_show(int *arr, size_t lenght) {
 
-    int arr[] = {3, 2, 1};
-    //bubble_sort(arr, 3, sizeof(int), comp);
-    quick_sort(arr, 3, sizeof(int), comp);
-    for (size_t i = 0; i < 3; i++) {
-        printf("%d\n", arr[i]);
-    }
+    for (size_t i = 0; i < lenght; ++i)
+        printf("%d, ", arr[i]);
 
-    return 0;
+    printf("\n\n");
 }
--- sort/src/quick_sort.c
+++ sort/src/quick_sort.c
@@ -12,17 +12,38 @@ void quick_sort(void *arr, size_t n_member, size_t size_m, ICOMPARATOR) {
     if (n_member <= 1)
         return;
 
-    char v;
-    scanf("%c", &v);
     size_t pivot_f = quick_partition(arr, n_member, size_m, comparator);
-    printf("%lu - %lu\n", pivot_f, n_member);
+
     quick_sort(arr, pivot_f, size_m, comparator);
     quick_sort(((char *)arr) + SHIFTBYTEIDXSIZEM(pivot_f + 1, size_m),
-               n_member - pivot_f, size_m, comparator);
+               n_member - pivot_f - 1, size_m, comparator);
 }
 
 size_t quick_partition(void *arr, size_t e, size_t size_m, ICOMPARATOR) {
 
     char *arr_c = (char *)arr;
     size_t i = 1, j = e - 1;
+
+    while (i < j) {
+
+        while (i < j && comparator(&arr_c[SHIFTBYTEIDXSIZEM(i, size_m)],
+                                   &arr_c[0]) <= 0) {
+            i++;
+        }
+
+        while (comparator(&arr_c[SHIFTBYTEIDXSIZEM(j, size_m)], &arr_c[0]) >
+               0) {
+            j--;
+        }
+
+        if (i < j) {
+            SWAPVARIABLE(&arr_c[SHIFTBYTEIDXSIZEM(i, size_m)],
+                         &arr_c[SHIFTBYTEIDXSIZEM(j, size_m)], size_m);
+        }
+    }
+
+    if (comparator(&arr_c[SHIFTBYTEIDXSIZEM(j, size_m)], &arr_c[0]) < 0)
+        SWAPVARIABLE(&arr_c[0], &arr_c[SHIFTBYTEIDXSIZEM(j, size_m)], size_m);
+
+    return j;
 }
