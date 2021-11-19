7b2481bfa0508bdcdf705cf1a4f65fb451f4b6da
Ramon Almeida <ramongoncalves76@gmail.com>
2021-11-19 00:08:12 -0300

    Return of divmodular changed to size_t.

--- math/include/arithmetic.h
+++ math/include/arithmetic.h
@@ -1,12 +1,14 @@
 /**
  * @author Ytalo Ramon
  * @date   12/06/2021
-*/
+ */
+
+#include "stdlib.h"
 
 /**
  * Calculate modular arithmetic.
  * @param a value
  * @param b div_value
  * @return remainder
-*/
-int divmodular(int a, int b);
+ */
+size_t divmodular(int a, int b);
--- math/src/arithmetic.c
+++ math/src/arithmetic.c
@@ -1,14 +1,14 @@
 /**
  * @author Ytalo Ramon
  * @date 13/06/2021
- * 
-*/
+ *
+ */
 
 #include "../include/arithmetic.h"
 
-int divmodular(int a, int b){
-    
+size_t divmodular(int a, int b) {
+
     int v = a % b;
-    
+
     return v >= 0 ? v : v + b;
 }
