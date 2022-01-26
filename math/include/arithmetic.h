30ec744e4de562b9a79fc0b7ab10ae88aa6c8123
Ramon Almeida <ramongoncalves76@gmail.com>
2022-01-25 23:39:31 -0300

    Added min and max macro.

--- math/include/arithmetic.h
+++ math/include/arithmetic.h
@@ -6,7 +6,20 @@
 #include "stdlib.h"
 
 /**
+ * Returns the item with the lowest value.
+ *
+ */
+#define min(a, b) (((a) <= (b) ? (a) : (b)))
+
+/**
+ * Returns the item with the highest value.
+ *
+ */
+#define max(a, b) (((a) >= (b) ? (a) : (b)))
+
+/**
  * Calculate modular arithmetic.
+ *
  * @param a value
  * @param b div_value
  * @return remainder
