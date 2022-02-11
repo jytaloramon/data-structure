99bb52e8eb0949c9041f41380c621446fe64de00
Ramon Almeida <ramongoncalves76@gmail.com>
2022-02-10 23:49:49 -0300

    Heapb refactoring.

--- heap/binary-heap/include/heap_b.h
+++ heap/binary-heap/include/heap_b.h
@@ -4,91 +4,106 @@
  */
 
 #include "../../../includes/icomparator.h"
-#include "../../../includes/itree_array.h"
+#include "stdlib.h"
 
-typedef struct _Heap Heap;
+typedef struct _HeapB HeapB;
 
-typedef struct _ItemHeap ItemHeap;
+typedef struct _HeapBItem HeapBItem;
 
-struct _ItemHeap {
-    void *key;
+struct _HeapBItem {
+    char padding;
 };
 
-struct _Heap {
-    struct _ItemHeap **arr;
-    int lenght, size;
+struct _HeapB {
+    struct _HeapBItem **arr;
+    size_t lenght, size;
 };
 
 /**
- * Create a new Heap structure.
- * @param arr heap, array of pointers to items.
+ * Create a new Binary Heap (HeapB) Structure.
+ *
+ * @param arr heap array, array of pointers HeapBItem.
  * @param length number of items in array.
  * @param size maximum array capacity.
  * @return A Heap instance or Null in case of error.
  */
-Heap *heapb_new(ItemHeap **arr, int length, int size);
+HeapB *heapb_new(HeapBItem **arr, size_t length, size_t size);
 
 /**
  * Build a heap.
- * @param heap Heap instance.
- * @param comparator Function comparator.
- * @return Or 1 on success, or 0 on failure.
+ *
+ * @param heap HeapB instance.
+ * @param comparator Comparison function.
+ *  Input in comparator(HeapBItem, HeapBItem).
+ * @return Or 1 on success or 0 on failure.
  */
-int heapb_build(Heap *heap, ICOMPARATOR);
+int heapb_build(HeapB *heap, ICOMPARATOR);
 
 /**
  * Checks if the heap is empty.
- * @param heap Heap instance.
+ *
+ * @param heap HeapB instance.
  * @return Or 1 if empty or 0 if not empty.
  */
-int heapb_is_empty(Heap *heap);
+int heapb_is_empty(HeapB *heap);
 
 /**
  * Checks if the heap is full.
- * @param heap Heap instance.
+ *
+ * @param heap HeapB instance.
  * @return Or 1 if full or 0 if not full.
  */
-int heapb_is_full(Heap *heap);
+int heapb_is_full(HeapB *heap);
 
 /**
- * Heapify bottom-up
+ * Heapify bottom-up.
+ *
+ * @param arr heap array, array of pointers HeapBItem.
  * @param posi Position of the reference element in the array, start of heapify.
- * @param arr heap, array of pointers to items.
- * @param comparator Function comparator.
- * @return Final position of the process.
+ * @param comparator Comparison function.
+ *  Input in comparator(HeapBItem, HeapBItem).
+ * @return Final item position.
  */
-int heapb_heapify_up(int posi, ItemHeap **arr, ICOMPARATOR);
+size_t heapb_heapify_up(HeapBItem **arr, size_t posi, ICOMPARATOR);
 
 /**
  * Heapify top-down.
+ *
+ * @param arr heap array, array of pointers HeapBItem.
  * @param posi Position of the reference element in the array, start of heapify.
- * @param arr heap, array of pointers to items.
  * @param length Length of array.
- * @param comparator Function comparator.
- * @return Final position of the process.
+ * @param comparator Comparison function.
+ *  Input in comparator(HeapBItem, HeapBItem).
+ * @return Final item position.
  */
-int heapb_heapify_down(int posi, ItemHeap **arr, int length, ICOMPARATOR);
+size_t heapb_heapify_down(HeapBItem **arr, size_t posi, size_t length,
+                       ICOMPARATOR);
 
 /**
- * Insert a new element.
- * @param heap Heap instance.
- * @param item Element Heap for insertion.
- * @param comparator Function comparator.
- * @return Or 1 on success, or 0 on failure.
+ * Insert a new item in the heap.
+ *
+ * @param heap HeapB instance.
+ * @param new_item HeapBItem for insertion.
+ * @param comparator Comparison function.
+ *  Input in comparator(HeapBItem, HeapBItem).
+ * @return Or 1 on success or 0 on failure.
  */
-int heapb_insert(Heap *heap, ItemHeap *item, ICOMPARATOR);
+int heapb_insert(HeapB *heap, HeapBItem *new_item, ICOMPARATOR);
 
 /**
- * Remove the top heap element.
- * @param heap Heap instance.
- * @param comparator Function comparator.
+ * Remove the top item of the heap.
+ * 
+ * @param heap HeapB instance.
+ * @param comparator Comparison function.
+ *  Input in comparator(HeapBItem, HeapBItem).
  * @return Or the instance removed, or NULL on failure.
  */
-ItemHeap *heapb_remove(Heap *heap, ICOMPARATOR);
+HeapBItem *heapb_remove(HeapB *heap, ICOMPARATOR);
 
 /**
- * Return the top heap element
- * @param heap Heap instance.
- * @return Or the instance of the first element, or NULL on failure.
+ * Return the top item heap.
+ * 
+ * @param HeapB Heap instance.
+ * @return or the top item or NULL on failure.
  */
-ItemHeap *heapb_peek(Heap *heap);
+HeapBItem *heapb_peek(HeapB *heap);
--- heap/binary-heap/src/heap_b.c
+++ heap/binary-heap/src/heap_b.c
@@ -3,104 +3,105 @@
  * @date   09/08/2021
  */
 
-#include "stdlib.h"
 #include "../include/heap_b.h"
+#include "../../../includes/itree_array.h"
+#include "stdlib.h"
 
-#define SWAPPOINTER(s, t)                                                      \
+#define HPSWAPPOINTER(s, t)                                                      \
     do {                                                                       \
         void *aux = s;                                                         \
         s = t;                                                                 \
         t = aux;                                                               \
     } while (0);
 
-Heap *heapb_new(ItemHeap **arr, int length, int size) {
+HeapB *heapb_new(HeapBItem **arr, size_t length, size_t size) {
 
-    Heap *heap = malloc(sizeof(Heap));
+    HeapB *heap = malloc(sizeof(HeapB));
 
-    if (!heap)
-        return NULL;
-
-    heap->arr = arr;
-    heap->lenght = length;
-    heap->size = size;
+    if (heap) {
+        heap->arr = arr;
+        heap->lenght = length;
+        heap->size = size;
+    }
 
     return heap;
 }
 
-int heapb_build(Heap *heap, ICOMPARATOR) {
+int heapb_build(HeapB *heap, ICOMPARATOR) {
 
     if (heap->lenght < 2)
         return 1;
 
-    for (int i = FATHER(heap->lenght - 1); i >= 0; i--)
-        heapb_heapify_down(i, heap->arr, heap->lenght, comparator);
+    for (int i = FATHER(heap->lenght - 1); i >= 0; --i)
+        heapb_heapify_down(heap->arr, i, heap->lenght, comparator);
 
     return 1;
 }
 
-int heapb_is_empty(Heap *heap) { return heap->lenght == 0; }
+int heapb_is_empty(HeapB *heap) { return heap->lenght == 0; }
 
-int heapb_is_full(Heap *heap) { return heap->lenght == heap->size; }
+int heapb_is_full(HeapB *heap) { return heap->lenght == heap->size; }
 
-int heapb_heapify_up(int posi, ItemHeap **arr, ICOMPARATOR) {
+size_t heapb_heapify_up(HeapBItem **arr, size_t posi, ICOMPARATOR) {
 
     int i = posi, f = FATHER(i);
 
-    while (i > 0 && comparator(arr[i]->key, arr[f]->key) > 0) {
-        SWAPPOINTER(arr[f], arr[i]);
+    while (i > 0 && comparator(arr[i], arr[f]) > 0) {
+        HPSWAPPOINTER(arr[f], arr[i]);
         i = f;
-        f = FATHER(i);
+        f = FATHER(f);
     }
 
     return i;
 }
 
-int heapb_heapify_down(int posi, ItemHeap **arr, int length, ICOMPARATOR) {
+size_t heapb_heapify_down(HeapBItem **arr, size_t posi, size_t length,
+                          ICOMPARATOR) {
 
     int i = posi, idx_min_child = 0;
 
     while ((idx_min_child = CHILDLEFT(i)) < length) {
 
-        if (CHILDRIGHT(i) < length) {
-            idx_min_child =
-                comparator(arr[CHILDLEFT(i)]->key, arr[CHILDRIGHT(i)]->key) >= 0
-                    ? CHILDLEFT(i)
-                    : CHILDRIGHT(i);
+        if (CHILDRIGHT(i) < length &&
+            comparator(arr[CHILDLEFT(i)], arr[CHILDRIGHT(i)]) < 0) {
+            idx_min_child = CHILDRIGHT(i);
         }
 
-        if (comparator(arr[idx_min_child]->key, arr[i]->key) <= 0)
+        if (comparator(arr[idx_min_child], arr[i]) <= 0)
             return i;
 
-        SWAPPOINTER(arr[idx_min_child], arr[i]);
+        HPSWAPPOINTER(arr[idx_min_child], arr[i]);
         i = idx_min_child;
     }
 
     return FATHER(i);
 }
 
-int heapb_insert(Heap *heap, ItemHeap *item, ICOMPARATOR) {
+int heapb_insert(HeapB *heap, HeapBItem *new_item, ICOMPARATOR) {
 
-    if (heapb_is_full(heap) || !item)
-        return -1;
+    if (heapb_is_full(heap) || !new_item)
+        return 0;
 
-    heap->arr[heap->lenght++] = item;
+    heap->arr[heap->lenght++] = new_item;
+    heapb_heapify_up(heap->arr, heap->lenght - 1, comparator);
 
-    return heapb_heapify_up(heap->lenght - 1, heap->arr, comparator);
+    return 1;
 }
 
-ItemHeap *heapb_remove(Heap *heap, ICOMPARATOR) {
+HeapBItem *heapb_remove(HeapB *heap, ICOMPARATOR) {
 
     if (heapb_is_empty(heap))
         return NULL;
 
-    void *elmnt = heap->arr[0];
-    heap->arr[0] = heap->arr[--heap->lenght];
-    heapb_heapify_down(0, heap->arr, heap->lenght, comparator);
+    HeapBItem *item_rm = heap->arr[0];
 
-    return elmnt;
+    heap->arr[0] = heap->arr[--heap->lenght];
+    heapb_heapify_down(heap->arr, 0, heap->lenght, comparator);
+
+    return item_rm;
 }
 
-ItemHeap *heapb_peek(Heap *heap) {
+HeapBItem *heapb_peek(HeapB *heap) {
 
     if (heapb_is_empty(heap))
         return NULL;
